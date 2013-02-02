eb1b3a9bca661035c6a0d07685705990b97dbbd4
expell3dcraft <expell3dcraft@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2013-02-02 20:55:44 +0000

    This stuff is rally cool and stuff, it has the new code for the climbing subsystem including the Solenoid.class and Climb.class

--- trunk/UltimateAscent/Robot2013/src/org/usfirst/frc3950/Robot2013/commands/Climb.java
+++ trunk/UltimateAscent/Robot2013/src/org/usfirst/frc3950/Robot2013/commands/Climb.java
@@ -28,6 +28,7 @@ public class  Climb extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.climber.setClimbScrewSpeed(Robot.oi.getClimberSpeed());
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
