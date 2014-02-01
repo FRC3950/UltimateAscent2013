af7d794fd0e637d2fcc2937a645c08d153e65232
rs.rsaccone@gmail.com <rs.rsaccone@gmail.com@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2014-02-01 18:43:25 +0000

    Further refinement of Mecanum drive code to include gryro capability, filtering and smoothing so movements aren't abrupt.

---
+++ DesperateRobot/UtilFun.h
@@ -0,0 +1,17 @@
+#ifndef UTIL_FUN_H
+#define UTIL_FUN_H
+
+/*
+ * if the value is betwen the min and the max it sets it to 0 
+ * otherwize it returns the original value
+ */
+inline float ZeroIfInRangeInclusive(float value, float min, float max) {
+	if ((value >= min) && (value <= max))
+	{
+		return 0.0;
+	}
+	
+	return value;
+}
+
+#endif // UTIL_FUN_H
