c8a0a0dd060ae53388577959062bb06ee2f6f759
rs.rsaccone@gmail.com <rs.rsaccone@gmail.com@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2014-02-19 01:10:39 +0000

    Changes and adjustments to behavior determined when actually using the robot.

--- DesperateRobot/Commands/AutonomousCommand.cpp
+++ DesperateRobot/Commands/AutonomousCommand.cpp
@@ -1,9 +1,9 @@
 #include "AutonomousCommand.h"
 
 AutonomousCommand::AutonomousCommand() {
-	AddSequential(new AutoMoveForward());
-	AddSequential(new LaunchAngle());
-	AddSequential(new OpenClaw(true));
-	AddSequential(new Fire());
+	AddSequential(new AutoMoveForward()); // this moves the robot forward and stops it after a set period of time 
+	AddSequential(new LaunchAngle());     // this puts the arm in to shooting position
+	AddSequential(new OpenClaw(true));    // this opens the claw to alow the ball to be launched
+	AddSequential(new Fire());            // this releses the trigger and resets the shooter
 }
 	
