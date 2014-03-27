10d2bd6dee3c0e50353f00f6b77f81a449c153ec
rs.rsaccone@gmail.com <rs.rsaccone@gmail.com@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2014-03-27 01:50:51 +0000

    Added vision targeting support to AutonomousCommand.

--- DesperateRobot/LoggingComponentDefs.h
+++ DesperateRobot/LoggingComponentDefs.h
@@ -9,8 +9,9 @@ const unsigned int ArmPIDSubsystemLogId 	= 1U << 1;  //0x00000002U;
 const unsigned int DriveSubsystemLogId 		= 1U << 2;  //0x00000004U;
 const unsigned int IntakeSubsystemLogId 	= 1U << 3;  //0x00000008U;
 const unsigned int PneumaticsSubsystemLogId	= 1U << 4;  //0x00000010U;
-const unsigned int ShooterSubsystemLogId	= 1U << 5;	//0x00000020U; 
+const unsigned int ShooterSubsystemLogId	= 1U << 5;	//0x00000020U;
+const unsigned int VisionTargetingId		= 1U << 6;	//0x00000040U;
 
-const unsigned int DefaultLoggingMask = (RobotLogId | DriveSubsystemLogId);
+const unsigned int DefaultLoggingMask = (RobotLogId | DriveSubsystemLogId | VisionTargetingId);
 const Logger::MessageType DefaultLoggingLevel = Logger::kINFO;
 #endif
