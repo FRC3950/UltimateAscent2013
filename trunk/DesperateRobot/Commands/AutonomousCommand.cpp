6a6693d696e4346e17f2e613509f629c29abfaee
rs.rsaccone@gmail.com <rs.rsaccone@gmail.com@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2014-03-27 01:50:07 +0000

    Added vision targeting support to AutonomousCommand.

--- trunk/DesperateRobot/Commands/AutonomousCommand.cpp
+++ trunk/DesperateRobot/Commands/AutonomousCommand.cpp
@@ -1,7 +1,19 @@
 #include "AutonomousCommand.h"
 
+#include "AutoMoveForward.h"
+#include "LaunchAngle.h"
+#include "Fire.h"
+#include "OpenClaw.h"
+#include "StallCommand.h"
+#include "VisionTargeting.h"
+
 AutonomousCommand::AutonomousCommand() {
+	StallCommand * stallCommand = new StallCommand();
+	VisionTargeting *visionTargeting = new VisionTargeting(stallCommand);
+	
+	AddSequential(visionTargeting);		  // checks for a target detection using vision.
 	AddSequential(new AutoMoveForward()); // this moves the robot forward and stops it after a set period of time 
+	AddSequential(stallCommand);		  // this command stalls for a configurable amount of time (stall time set by vision);
 	AddSequential(new LaunchAngle());     // this puts the arm in to shooting position
 	AddSequential(new OpenClaw(true));    // this opens the claw to alow the ball to be launched
 	AddSequential(new Fire());            // this releses the trigger and resets the shooter
