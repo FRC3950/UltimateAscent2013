10908dbe30f41079f898c7e0a98fe844166c246d
rs.rsaccone@gmail.com <rs.rsaccone@gmail.com@58029ea3-ec78-6f80-4d9f-db291f277cc6>
2015-02-17 04:22:17 +0000

    Lots of changes and experiments to get autonomous working

--- RecycleRush/src/Commands/Autonomous_1RS_Command.cpp
+++ RecycleRush/src/Commands/Autonomous_1RS_Command.cpp
@@ -6,6 +6,7 @@
 #include "OpenGripCommand.h"
 #include "ToggleGripCommand.h"
 #include "AutoDriveCommand.h"
+#include "StallCommand.h"
 #include "../ConfigKeys.h"
 #include "../Config/ConfigInstanceMgr.h"
 
@@ -16,8 +17,12 @@ Autonomous_1RS_Command::Autonomous_1RS_Command()
 	ConfigMgr *configMgr = ConfigInstanceMgr::getInstance();
 
 	AddSequential(new CloseGripCommand());
+	AddSequential(new StallCommand(0.1));
 	AddSequential(new LiftUpCommand());
+	AddSequential(new StallCommand(0.5));
 	AddSequential(new AutoDriveCommand(DriveLeft, configMgr->getDoubleVal(ConfigKeys::Auto1RS_MoveLeftDistance_Key, Auto1RS_MoveLeftDistanceDefault)));
+	AddSequential(new StallCommand(0.5));
 	AddSequential(new DropDownCommand());
+	AddSequential(new StallCommand(0.5));
 	AddSequential(new OpenGripCommand());
 }
